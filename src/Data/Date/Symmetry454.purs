2c178346962a786db12fbab4ed6ec2ab054b68ce
Pierre Beaucamp <mail@pierrebeaucamp.com>
2016-02-19 15:13:31 -0500

    Minor changes

--- src/Data/Date/Symmetry454.purs
+++ src/Data/Date/Symmetry454.purs
@@ -1,18 +1,32 @@
 module Data.Date.Symmetry454 (
     checkForLeapWeek,
-    estimateSymYear,
+    estimateYear,
     fixedToSym,
     fixedToSymYear,
-    isSymLeap,
-    symDayToMonth,
+    isLeap,
+    daysToMonth,
     symMonthToDay,
-    symNewYearDay
+    newYearDay,
+    Year(..)
 ) where
 
-import Data.Date (Year(..))
-import Data.Int (ceil, floor, toNumber)
+import Data.Int (ceil, toNumber)
 import Math ((%))
 import Prelude
+
+{-|
+  | A Symmetry454 year
+-}
+newtype Year = Year Int
+instance eqYear :: Eq Year where eq (Year x) (Year y) = x == y
+instance ordYear :: Ord Year where compare (Year x) (Year y) = compare x y
+instance ringYear :: Ring Year where sub (Year x) (Year y) = Year (x - y)
+instance semiringYear :: Semiring Year where
+    add (Year x) (Year y) = Year (x + y)
+    mul (Year x) (Year y) = Year (x * y)
+    zero = Year zero
+    one = Year one
+instance showYear :: Show Year where show (Year x) = show x
 
 {-|
   | checkForLeapWeek gets called if fixedToSymYear holds no simple solution.
@@ -20,30 +34,29 @@ import Prelude
 -}
 checkForLeapWeek :: Int -> Int -> Year
 checkForLeapWeek fixed start = do
-    let nextYear = estimateSymYear fixed + Year 1
-    if fixed - start >= 364 && fixed >= symNewYearDay(nextYear)
+    let nextYear = estimateYear fixed + Year 1
+    if fixed - start >= 364 && fixed >= newYearDay(nextYear)
         then nextYear
-        else estimateSymYear fixed
+        else estimateYear fixed
 
 {-|
-  | estimateSymYear estimates the Symmetry454 calendar year given a fixed date
+  | estimateYear estimates the Symmetry454 calendar year given a fixed date
   | number.
 -}
-estimateSymYear :: Int -> Year
-estimateSymYear fixed = Year $ ceil $ (f - 1.0) / (365.0 + 71.0 / 293.0) where
-    f = toNumber fixed
+estimateYear :: Int -> Year
+estimateYear fixed = do
+    Year $ ceil $ (toNumber fixed - 1.0) / (365.0 + 71.0 / 293.0)
 
 {-|
   | fixedToSym converts any fixed day number to the corresponding Symmetry454
   | calendar date.
 -}
 fixedToSym :: Int -> String
-fixedToSym fixed = show month ++ "/" ++ show day ++ "/" ++ yearString where
-    year       = fixedToSymYear fixed $ symNewYearDay $ estimateSymYear fixed
-    dayOfYear  = fixed - (symNewYearDay year) + 1
-    month      = symDayToMonth dayOfYear
+fixedToSym fixed = show month ++ "/" ++ show day ++ "/" ++ show year where
+    year       = fixedToSymYear fixed $ newYearDay $ estimateYear fixed
+    dayOfYear  = fixed - (newYearDay year) + 1
+    month      = daysToMonth dayOfYear
     day        = symMonthToDay dayOfYear month
-    yearString = case year of Year x -> show x
 
 {-|
   | fixedToSymYear returns the current year in the Symmetry454 calendar that
@@ -52,24 +65,24 @@ fixedToSym fixed = show month ++ "/" ++ show day ++ "/" ++ yearString where
 -}
 fixedToSymYear :: Int -> Int -> Year
 fixedToSymYear fixed start
-    | fixed < start  = estimateSymYear fixed - Year 1
+    | fixed < start  = estimateYear fixed - Year 1
     | fixed > start  = checkForLeapWeek fixed start
-    | otherwise      = estimateSymYear fixed
+    | otherwise      = estimateYear fixed
 
 {-|
-  | isSymLeap checks if the provided year is a leap year in the Symmetry454
-  | calendar.
+  | isLeap checks if the provided year is a leap year.
 -}
-isSymLeap :: Year -> Boolean
-isSymLeap year = (52.0 * y + 146.0) % 293.0 < 52.0 where
-    y = case year of Year x -> toNumber x
+isLeap :: Year -> Boolean
+isLeap year = do
+    let y = case year of Year x -> toNumber x
+    (52.0 * y + 146.0) % 293.0 < 52.0
 
 {-|
-  | symDayToMonth returns the current month of the Symmetry454 calendar given
+  | daysToMonth returns the current month of the Symmetry454 calendar given
   | the ordinal number of days in a year.
 -}
-symDayToMonth :: Int -> Int
-symDayToMonth dayOfYear = 3 * quarter + monthOfQuarter - 3 where
+daysToMonth :: Int -> Int
+daysToMonth dayOfYear = 3 * quarter + monthOfQuarter - 3 where
     weekOfYear     = ceil $ toNumber(dayOfYear) / 7.0
     quarter        = ceil $ 4.0 / 53.0 * toNumber(weekOfYear)
     dayOfQuarter   = dayOfYear - 91 * (quarter - 1)
@@ -81,13 +94,15 @@ symDayToMonth dayOfYear = 3 * quarter + monthOfQuarter - 3 where
   | ordinal number of days of the current year and the current month.
 -}
 symMonthToDay :: Int -> Int -> Int
-symMonthToDay dayOfYear month = dayOfYear - daysBefore where
-    daysBefore = 28 * month + 7 * month / 3 - 28
+symMonthToDay dayOfYear month = do
+    let daysBefore = 28 * month + 7 * month / 3 - 28
+    dayOfYear - daysBefore
 
 {-|
-  | symNewYearDay returns the fixed day number of the New Year Day.
+  | newYearDay returns the fixed day number of the New Year Day.
 -}
-symNewYearDay :: Year -> Int
-symNewYearDay year = 1 + 364 * p + 7 * (52 * p + 146) / 293 where
-    p = case year of Year x -> x - 1
+newYearDay :: Year -> Int
+newYearDay year = do
+    let p = case year of Year x -> x - 1
+    1 + 364 * p + 7 * (52 * p + 146) / 293
 
